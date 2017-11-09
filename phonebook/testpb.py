a6ebe9552662abc2d170feb3cbcab69b32f9797a
Alexander Kozarev <al1102@yandex.ru>
2017-11-09 17:53:04 +0500

    pb

--- phonebook/testpb.py
+++ phonebook/testpb.py
@@ -1,11 +1,15 @@
-def a():
+def add():
     print("OK!")
+	
+def exit():
+	...
 
 while(True):
     inp = input("just print something \n" )
     message = inp.split(" ")
-    if message[0] == 'a':
-        a()
+    if message[0] == 'добавить':
+        add()
+	#для восприятия русского текста посмотреть pep-0263
 
     elif message[0] == 'exit':
         break
