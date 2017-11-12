150d9e41d085704714779cfdfb7cc91ab91b50b7
Alexander <al1102@yandex.ru>
2017-11-13 00:52:51 +0500

    pb

--- phonebook/testpb.py
+++ phonebook/testpb.py
@@ -4,33 +4,57 @@
 import pymysql
 
 # Open database connection
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='toshi',
-                     db='testdb')
+conn = pymysql.connect(host='localhost',
+                       port=3306,
+                       user='root',
+                       passwd='3720011',
+                       db='testdb',
+                       charset = "utf8",
+                       use_unicode = True)
 cur = conn.cursor()
 
-# Drop table if it already exist using execute() method.
 cur.execute("DROP TABLE IF EXISTS PB")
 
-"""Creating Database Table"""
 sql = """CREATE TABLE PB (
          NUMBER CHAR(20) NOT NULL,
 		 NAME  CHAR(20) NOT NULL
          )"""
 cur.execute(sql)
 
+
+sql = """INSERT INTO PB (NUMBER, NAME)
+         VALUES ('+79221234567', 'Bob Marley')
+         """
+try:
+    cur.execute(sql)
+    print("OK!")
+except:
+    conn.rollback()
+
+
+sql = "SELECT * FROM PB"
+try:
+   cur.execute(sql)
+   results = cur.fetchall()
+   for row in results:
+      number = row[0]
+      name = row[1]
+      print ("number=%s,name=%s" % \
+             (number, name))
+except:
+	print( "error: unable to fecth data")
+
+
 def add():
-	print("try")
-	"""INSERT Operation"""
-	sql = """INSERT INTO EMPLOYEE(NUMBER, NAME)
-         VALUES ('+79221234567', 'Иван Петров')"""
+	print("try...")
+	sql = """INSERT INTO PB (NUMBER, NAME)
+             VALUES ('+79221234567', 'Bob Marley')
+             """
 	try:
 		cur.execute(sql)
 		print("OK!")
 	except:
 		conn.rollback()
-	
-def exit():
-	...
 
 while(True):
     inp = input("just print something \n" )
@@ -41,4 +65,3 @@ while(True):
 
     elif message[0] == 'выход':
         break
-    
